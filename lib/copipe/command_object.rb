6c581bdf917752ec9f05d1ead0888f1660182b5c
motsat <konpeiex@gmail.com>
2015-01-02 19:18:07 +0900

    コマンド結果を保存してコマンド置換する

--- lib/copipe/command_object.rb
+++ lib/copipe/command_object.rb
@@ -1,15 +1,27 @@
 module Copipe
   class CommandObject
-    attr_reader :command, :description, :need_confirm, :chdir
+    attr_reader :command, :description, :need_confirm, :chdir, :set_val
     def initialize(vars)
       @command      = vars["command"]
       @description  = vars["description"]
       @need_confirm = !vars["need_confirm"]
       @chdir        = vars["chdir"]
+      @set_val      = vars["set_val"]
+    end
+
+    def replace_command(val:val)
+      val.each_pair do |k, v|
+        @command.gsub! k, v
+      end
     end
 
     def need_confirm?
       @need_confirm
     end
+
+    def set_val_command?
+      !@set_val.nil?
+    end
+
   end
 end
--- lib/copipe/executer.rb
+++ lib/copipe/executer.rb
@@ -9,11 +9,11 @@ module Copipe
     def execute(file_name)
 
       inputs = {}
-      commands = Parser.parse_with_file(file_name:file_name)
+      @commands = Parser.parse_with_file(file_name:file_name)
 
       jump_index = -1 # jump制御
 
-      commands.each_with_index do |command, index|
+      @commands.each_with_index do |command, index|
 
         # jumpが設定されていたらskipしたことにする
         if (index < jump_index)
@@ -22,24 +22,41 @@ module Copipe
         end
 
         # show all comamnds
-        show_commands(commands:commands, current_index: index, inputs: inputs) # 1件実行ごとにコマンドを表示する
+        show_commands(current_index: index, inputs: inputs) # 1件実行ごとにコマンドを表示する
         # execute
         input = command.need_confirm?? Input.start : Input.execute_instance
-        if input.abort?
-          exit 1
-        elsif input.jump?
-          jump_index = (input.input_value - 1)
-        end
+        exit 1 if input.abort?
+        jump_index = (input.input_value - 1) if input.jump?
         inputs[index] = input
-        if input.execute?
-          Dir.chdir(command.chdir) if command.chdir
-          system command.command   if command.command
-        end
+        res = execute_command(command:command)  if input.execute?
+        replace_commands(set_val:res[:set_val]) if res[:set_val]
       end
     end
 
-    def show_commands(commands:commands, current_index:current_index, inputs:inputs)
-      commands.each_with_index do |command, index|
+    def replace_commands(set_val:set_val)
+      @commands.each_with_index do |command, i|
+        next unless command.command
+        @commands[i].replace_command(val:set_val)
+      end
+    end
+
+    def execute_command(command:command)
+      Dir.chdir(command.chdir) if command.chdir
+      return unless command.command
+      # set_valコマンドの時は標準出力を取りたいのでバッククオート実行
+      # その他は、都度出力されるものを表示したいのでsystemで実行
+      res = {set_val:nil}
+      if command.set_val_command?
+        val = `#{command.command}`.chomp
+        res[:set_val] = {command.set_val => val}
+      else
+        system command.command
+      end
+      res
+    end
+
+    def show_commands(current_index:current_index, inputs:inputs)
+      @commands.each_with_index do |command, index|
         puts LineDecorator.decoration line:command, index:index, current_index: current_index, input: inputs[index]
       end
     end
